d114baeeb3f1618f7af7c3cbc0e9a2e57f9eb4f6
Kasperoid <ivanu9053@gmail.com>
2024-10-13 13:00:43 +0300

    Создание типов для страны и темы

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+export type countryType = {
+    name: {
+        common: string,
+        nativeName: {
+            common: string
+        }
+    },
+    flags: {
+        png: string,
+        svg: string,
+    },
+    capital: string[],
+    region: string,
+    population: number,
+    tld?: string[],
+    currencies?: {
+        name: string,
+    }
+    subregion?: string,
+    languages?: unknown,
+    borders?: null | string[],
+    ccn3: number;
+}
+
+export type themeModeType = {
+    mode: 'light' | 'dark';
+}
