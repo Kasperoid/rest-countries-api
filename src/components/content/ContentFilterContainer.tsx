cf67e20de5f87ad03a4be8f00871b819ac49f8bb
Kasperoid <ivanu9053@gmail.com>
2024-10-13 14:58:14 +0300

    Вынос функций в отдельные файлы, импорт этих функций. Создание функций отлова событий поиска страны и выбора категории

--- src/components/content/ContentFilterContainer.tsx
+++ src/components/content/ContentFilterContainer.tsx
@@ -1,27 +1,29 @@
 import { Flex } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
 import useSWR from 'swr';
-import { fetcherCategory } from '../../helpers/fetcher';
+import { fetcherCategory, fetcherCountry } from '../../helpers/fetcher';
 import { URL_ALL_COUNTRIES } from '../../constants/constants';
 import { Context } from '../../App';
 import { useContext } from 'react';
 import { InputSearchStyled } from '../../styles/content/filters/InputSearchStyled';
 import { SelectCountryStyled } from '../../styles/content/filters/SelectCountryStyled';
 
-type optionSelect = {
-  value: string;
-  label: string;
-};
-
 export const ContentFilterContainer = () => {
-  const optionsSelectCreate = (options: string[]): optionSelect[] => {
-    const result = options.map(item => {
-      return {
-        value: item,
-        label: item,
-      };
-    });
-    return result;
+  const onSelectCountryHandler = (selectText: string): void => {
+    const searchUrl =
+      selectText === 'All'
+        ? URL_ALL_COUNTRIES
+        : `https://restcountries.com/v3.1/region/${selectText}`;
+
+    mutate(() => fetcherCountry(searchUrl, {}), { revalidate: false });
+  };
+
+  const onSearchInputHandler = (searchTextCountry: string) => {
+    const searchUrl =
+      searchTextCountry.trim() === ''
+        ? URL_ALL_COUNTRIES
+        : `https://restcountries.com/v3.1/name/${searchTextCountry}`;
+
+    mutate(() => fetcherCountry(searchUrl, {}), { revalidate: false });
   };
 
   const { data, isLoading } = useSWR('countries-category', () =>
@@ -29,25 +31,27 @@ export const ContentFilterContainer = () => {
       fields: 'region',
     })
   );
-  const { isDarkMode } = useContext(Context);
 
-  const optionsSelect = data ? optionsSelectCreate(data) : undefined;
+  const { mutate } = useSWR('all-countries');
+  const { isDarkMode } = useContext(Context);
 
   return (
     <Flex justify="space-between">
       <InputSearchStyled
         mode={isDarkMode ? 'dark' : 'light'}
-        prefix={<SearchOutlined />}
         placeholder="Search for a country..."
         variant="borderless"
+        size="large"
+        onSearch={searchTextCountry => onSearchInputHandler(searchTextCountry)}
       />
       <div>
         <SelectCountryStyled
           loading={isLoading}
           placeholder="Filter by Region"
           optionFilterProp="label"
-          options={optionsSelect}
+          options={data}
           mode={isDarkMode ? 'dark' : 'light'}
+          onSelect={selectText => onSelectCountryHandler(selectText)}
         />
       </div>
     </Flex>
