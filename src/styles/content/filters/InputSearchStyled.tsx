7cb1b9699baf3ff71817c04b7a9483f7c612071e
Kasperoid <ivanu9053@gmail.com>
2024-10-13 14:56:55 +0300

    Изменение стиля для инпута под инпут серч

--- src/styles/content/filters/InputSearchStyled.tsx
+++ src/styles/content/filters/InputSearchStyled.tsx
@@ -1,25 +1,36 @@
-import { Input, InputProps } from 'antd';
+import { Input } from 'antd';
 import styled from 'styled-components';
 import { themeModeType } from '../../../types/types';
 import { baseTheme } from '../../baseTheme';
+import { SearchProps } from 'antd/es/input';
 
-export const InputSearchStyled = styled(Input)<InputProps & themeModeType>`
+const { Search } = Input;
+
+export const InputSearchStyled = styled(Search)<SearchProps & themeModeType>`
   && {
-    padding: 15px;
     box-shadow: 0px 0px 25px -3px rgba(34, 60, 80, 0.15);
     width: 30vw;
     min-width: 300px;
 
-    &.ant-input-affix-wrapper {
+    & .ant-input {
       background-color: ${({ mode }) => baseTheme.color[mode].element};
       color: ${({ mode }) => baseTheme.color[mode].text};
 
       & ::placeholder {
         color: ${({ mode }) => baseTheme.color[mode].input};
       }
+    }
 
-      & svg {
-        fill: ${({ mode }) => baseTheme.color[mode].input};
+    &.ant-input-search > .ant-input-group > .ant-input-group-addon:last-child {
+      & .ant-btn {
+        background-color: ${({ mode }) => baseTheme.color[mode].element};
+        border-color: ${({ mode }) => baseTheme.color[mode].background};
+        color: ${({ mode }) => baseTheme.color[mode].text};
+        transition: 0.5s ease border-color;
+
+        &:hover {
+          border-color: ${({ mode }) => baseTheme.color[mode].input};
+        }
       }
     }
   }
