56f1fe2bfccfbb5001555b67e00d08aadf4eb9f0
Kasperoid <ivanu9053@gmail.com>
2024-10-13 13:19:13 +0300

    Создание стиля для контейнера контента

---
+++ src/styles/content/ContentContainerStyled.tsx
@@ -0,0 +1,11 @@
+import { Layout } from 'antd';
+import styled from 'styled-components';
+
+const { Content } = Layout;
+export const ContentContainerStyled = styled(Content)`
+  && {
+    padding: 35px 55px;
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+`;
