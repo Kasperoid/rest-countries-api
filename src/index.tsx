7c6a66ac834c48e24738a43980d2ed79c3d467da
Kasperoid <ivanu9053@gmail.com>
2024-10-13 14:58:49 +0300

    Добавление глобальной конфигурации для SWR

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'normalize.css';
+import { SWRConfig } from 'swr';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SWRConfig
+      value={{
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+      }}
+    >
+      <App />
+    </SWRConfig>
   </React.StrictMode>
 );
