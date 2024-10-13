72c923397574483b09ead4bb6545ff5585d297c6
Kasperoid <ivanu9053@gmail.com>
2024-10-13 13:07:51 +0300

    Стилизация компоненты внутреннего контейнера шапки

---
+++ src/styles/header/HeaderContainerInnerStyled.tsx
@@ -0,0 +1,11 @@
+import { Flex, FlexProps } from 'antd';
+import styled from 'styled-components';
+
+export const HeaderContainerInnerStyled = styled(Flex)<FlexProps>`
+  && {
+    margin: 0 auto;
+    height: 100%;
+    max-width: 1440px;
+    padding: 0 55px;
+  }
+`;
