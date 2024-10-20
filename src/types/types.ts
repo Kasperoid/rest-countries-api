bd17a552fa29a37cbc353a6440881d1d947dfce7
Kasperoid <ivanu9053@gmail.com>
2024-10-21 00:20:56 +0300

    Создание нового типа для переделанного объекта страны

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,37 +1,65 @@
 export type CountryType = {
-    name: {
-        common: string,
-        nativeName: {
-            common: string
-        }
-    },
-    flags: {
-        png: string,
-        svg: string,
-    },
-    capital: string[],
-    region: string,
-    population: number,
-    tld?: string[],
-    currencies?: {
-        name: string,
-    }
-    subregion?: string,
-    languages?: unknown,
-    borders?: null | string[],
-    ccn3: number;
-}
+  name: {
+    common: string;
+    nativeName: {
+      [key: string]: {
+        common: string;
+      };
+    };
+  };
+  flags: {
+    png: string;
+    svg: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+  tld?: string[];
+  currencies?: {
+    [key: string]: {
+      name: string;
+    };
+  };
+  subregion?: string;
+  languages?: {
+    [key: string]: string;
+  };
+  borders?: null | string[];
+  ccn3: number;
+};
+
+export type CountryPageType = {
+  name: {
+    common: string;
+    nativeName: {
+      common: string;
+    };
+  };
+  flags: {
+    png: string;
+    svg: string;
+  };
+  capital: string;
+  region: string;
+  population: number;
+  tld?: string;
+  currencies?: string;
+  subregion?: string;
+  languages?: string;
+  borders?: null | string[];
+  ccn3: number;
+};
 
 export type ThemeModeType = {
-    mode: 'light' | 'dark';
-}
+  mode: 'light' | 'dark';
+};
 
 export type OptionSelect = {
-    value: string;
-    label: string;
+  value: string;
+  label: string;
 };
 
 export type ArgsFetcherType = {
-    url: string;
-    fields?: string;
-}
+  url: string;
+  fields?: string;
+};
