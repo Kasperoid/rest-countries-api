f610eec4230f3cd59b0f094faf36f9fec881cea8
Kasperoid <ivanu9053@gmail.com>
2024-10-21 00:22:11 +0300

    Применение функции перестройки объекта при получении

--- src/redux/slices/countrySlice.ts
+++ src/redux/slices/countrySlice.ts
@@ -1,43 +1,48 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { ArgsFetcherType, CountryType } from "../../types/types";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {
+  ArgsFetcherType,
+  CountryPageType,
+  CountryType,
+} from '../../types/types';
+import axios from 'axios';
+import { rebuildingCountry } from '../../helpers/rebuildingCountry';
 
 type StateType = {
-    isLoading: boolean,
-    country: CountryType | null
-}
+  isLoading: boolean;
+  country: CountryPageType | null;
+};
 
 const initialState: StateType = {
-    isLoading: true,
-    country: null
-}
+  isLoading: true,
+  country: null,
+};
 
-export const fetchCountry = createAsyncThunk<CountryType, ArgsFetcherType>
-    ('fetch/country',
-        async function (args) {
-            const { url, ...paramsQuery } = args
-            const resp = await axios(url, {
-                params: paramsQuery
-            });
-            const data = resp.data;
-            return data;
-        }
-    )
+export const fetchCountry = createAsyncThunk<CountryPageType, ArgsFetcherType>(
+  'fetch/country',
+  async function (args) {
+    const { url, ...paramsQuery } = args;
+    const resp = await axios<CountryType>(url, {
+      params: paramsQuery,
+    });
+    const data = rebuildingCountry(resp.data);
+    return data;
+  }
+);
 
 const countrySlice = createSlice({
-    name: 'country',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchCountry.pending, (state) => {
-            state.isLoading = true
-        })
+  name: 'country',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountry.pending, (state) => {
+      state.isLoading = true;
+    });
 
-        builder.addCase(fetchCountry.fulfilled, (state, action) => {
-            state.country = action.payload
-            state.isLoading = false
-        })
-    }
-})
+    builder.addCase(fetchCountry.fulfilled, (state, action) => {
+      state.country = action.payload;
+      state.isLoading = false;
+    });
+  },
+});
 
-export default countrySlice.reducer
+export default countrySlice.reducer;
