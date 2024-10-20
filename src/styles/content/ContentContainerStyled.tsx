e7464dce84ebac343125447ad08483ca2228ea09
Kasperoid <ivanu9053@gmail.com>
2024-10-20 15:33:27 +0300

    Изменение стиля для компонента обёртки контейнера с карточками стран

--- src/styles/content/ContentContainerStyled.tsx
+++ src/styles/content/ContentContainerStyled.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const { Content } = Layout;
 export const ContentContainerStyled = styled(Content)`
   && {
+    width: 100%;
     padding: 35px 55px;
     max-width: 1440px;
     margin: 0 auto;
