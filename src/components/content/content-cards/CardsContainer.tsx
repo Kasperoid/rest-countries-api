0ff5be0d446e831b9f4549087ca2a3b17e2e534f
Kasperoid <ivanu9053@gmail.com>
2024-10-20 15:35:56 +0300

    Убран useSwr, данные получаются из redux

--- src/components/content/content-cards/CardsContainer.tsx
+++ src/components/content/content-cards/CardsContainer.tsx
@@ -1,50 +1,55 @@
 import { Flex, Space } from 'antd';
-import useSWR from 'swr';
-import { countryType } from '../../../types/types';
-import { URL_ALL_COUNTRIES } from '../../../constants/constants';
-import { fetcherCountry } from '../../../helpers/fetcher';
 import { useContext } from 'react';
 import { Context } from '../../../App';
 import { CardCountryStyled } from '../../../styles/content/cards/CardCountryStyled';
 import { TitleStyled } from '../../../styles/TitleStyled';
 import { TextStyled } from '../../../styles/TextStyled';
+import { useAppSelector } from '../../../redux/hooks';
+import { SpinStyled } from '../../../styles/loader/SpinStyled';
+import { LoadingOutlined } from '@ant-design/icons';
 
 export const CardsContainer = () => {
   const { isDarkMode } = useContext(Context);
-  const { data } = useSWR<countryType[]>('all-countries', () =>
-    fetcherCountry(URL_ALL_COUNTRIES, {
-      fields: 'flags,population,region,capital,name,ccn3',
-    })
-  );
+  const data = useAppSelector(state => state.contries.countries);
+  const isLoading = useAppSelector(state => state.contries.isLoading);
 
   return (
-    <Flex wrap gap={50}>
-      {data?.map((item, index) => (
-        <CardCountryStyled
+    <>
+      {isLoading ? (
+        <SpinStyled
           mode={isDarkMode ? 'dark' : 'light'}
-          key={index}
-          hoverable
-          cover={<img alt={item.name.common} src={item.flags.svg} />}
-        >
-          <Flex vertical gap={10}>
-            <TitleStyled level={2}>{item.name.common}</TitleStyled>
-            <Flex vertical gap={5}>
-              <Space>
-                <TitleStyled level={3}>Population: </TitleStyled>
-                <TextStyled>{item.population}</TextStyled>
-              </Space>
-              <Space>
-                <TitleStyled level={3}>Region: </TitleStyled>
-                <TextStyled>{item.region}</TextStyled>
-              </Space>
-              <Space>
-                <TitleStyled level={3}>Capital: </TitleStyled>
-                <TextStyled>{item.capital}</TextStyled>
-              </Space>
-            </Flex>
-          </Flex>
-        </CardCountryStyled>
-      ))}
-    </Flex>
+          indicator={<LoadingOutlined spin />}
+        />
+      ) : (
+        <Flex wrap gap={50}>
+          {data?.map((item, index) => (
+            <CardCountryStyled
+              mode={isDarkMode ? 'dark' : 'light'}
+              key={index}
+              hoverable
+              cover={<img alt={item.name.common} src={item.flags.svg} />}
+            >
+              <Flex vertical gap={10}>
+                <TitleStyled level={2}>{item.name.common}</TitleStyled>
+                <Flex vertical gap={5}>
+                  <Space>
+                    <TitleStyled level={3}>Population: </TitleStyled>
+                    <TextStyled>{item.population}</TextStyled>
+                  </Space>
+                  <Space>
+                    <TitleStyled level={3}>Region: </TitleStyled>
+                    <TextStyled>{item.region}</TextStyled>
+                  </Space>
+                  <Space>
+                    <TitleStyled level={3}>Capital: </TitleStyled>
+                    <TextStyled>{item.capital}</TextStyled>
+                  </Space>
+                </Flex>
+              </Flex>
+            </CardCountryStyled>
+          ))}
+        </Flex>
+      )}
+    </>
   );
 };
