82829960b6fd80c71f922a1ef4580a8eb3650966
Kasperoid <ivanu9053@gmail.com>
2024-10-20 15:35:13 +0300

    Добавление редьюсеров из созданных слайсов

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import contriesSlice from './slices/countriesSlice'
+import categoryCountriesSlice from './slices/categoryContriesSlice'
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    contries: contriesSlice,
+    categories: categoryCountriesSlice
+  },
 });
 
 export default store;
