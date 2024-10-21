3b9377d8b06f3fbe6001af7db8ef44620e2440d5
Kasperoid <ivanu9053@gmail.com>
2024-10-22 00:24:40 +0300

    Добавление обработчика ошибок

--- src/redux/slices/countriesSlice.ts
+++ src/redux/slices/countriesSlice.ts
@@ -1,42 +1,56 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { ArgsFetcherType, CountryType } from "../../types/types";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { ArgsFetcherType, CountryType, ErrorType } from '../../types/types';
+import axios from 'axios';
 
 type CountriesState = {
-    countries: CountryType[] | null,
-    isLoading: boolean,
-}
+  countries: CountryType[] | null;
+  isLoading: boolean;
+  error: ErrorType | null;
+};
 
 const initialState: CountriesState = {
-    countries: null,
-    isLoading: true
-}
+  countries: null,
+  isLoading: true,
+  error: null,
+};
 
-
-export const fetchCountries = createAsyncThunk<CountryType[], ArgsFetcherType>
-    ('fetch/countries',
-        async function (args) {
-            const { url, ...paramsQuery } = args
-            const resp = await axios(url, {
-                params: paramsQuery
-            });
-            const data = resp.data;
-            return data;
-        })
+export const fetchCountries = createAsyncThunk<CountryType[], ArgsFetcherType>(
+  'fetch/countries',
+  async function (args, thunkApi) {
+    const { url, ...paramsQuery } = args;
+    const resp = await axios(url, {
+      params: paramsQuery,
+    });
+    try {
+      const data = resp.data;
+      return data;
+    } catch (e) {
+      return thunkApi.rejectWithValue(new Error('Ошибка'));
+    }
+  }
+);
 
 const countriesSlice = createSlice({
-    name: 'countries',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchCountries.pending, (state) => {
-            state.isLoading = true
-        })
-        builder.addCase(fetchCountries.fulfilled, (state, action) => {
-            state.countries = action.payload
-            state.isLoading = false
-        })
-    }
-})
+  name: 'countries',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountries.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchCountries.fulfilled, (state, action) => {
+      state.countries = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(fetchCountries.rejected, (state, action) => {
+      console.log(action);
+      state.error = {
+        code: action.error.code ?? '404',
+        message: action.error.message ?? 'Произошла ошибка',
+      };
+      state.isLoading = false;
+    });
+  },
+});
 
-export default countriesSlice.reducer
+export default countriesSlice.reducer;
