873a01abda08569d9b850d23a5b85fc6df1533d6
Kasperoid <ivanu9053@gmail.com>
2024-10-22 00:25:22 +0300

    Рефакторинг слайса с фильтрами

--- src/redux/slices/categoryContriesSlice.ts
+++ src/redux/slices/categoryContriesSlice.ts
@@ -1,44 +1,44 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { ArgsFetcherType, CountryType, OptionSelect } from "../../types/types";
-import axios from "axios";
-import { uniqRegion } from "../../helpers/uniqRegion";
-import { createSelectOptions } from "../../helpers/createSelectOptions";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { ArgsFetcherType, CountryType, OptionSelect } from '../../types/types';
+import axios from 'axios';
+import { uniqRegion } from '../../helpers/uniqRegion';
+import { createSelectOptions } from '../../helpers/createSelectOptions';
 
 type CategoryCountriesType = {
-    isLoading: boolean,
-    options: OptionSelect[] | []
-}
+  isLoading: boolean;
+  options: OptionSelect[] | [];
+};
 
 const initialState: CategoryCountriesType = {
-    isLoading: true,
-    options: []
-}
+  isLoading: true,
+  options: [],
+};
 
 export const fetchCategory = createAsyncThunk<OptionSelect[], ArgsFetcherType>(
-    'fetch/category',
-    async function (args) {
-        const { url, ...queryParams } = args
-        const resp = await axios(url, { params: queryParams })
-        const data: CountryType[] = resp.data
-        const uniqReg: string[] = uniqRegion(data)
-        const resultOptions: OptionSelect[] = createSelectOptions(uniqReg)
-        return resultOptions
-    }
-)
+  'fetch/category',
+  async function (args) {
+    const { url, ...queryParams } = args;
+    const resp = await axios(url, { params: queryParams });
+    const data: CountryType[] = resp.data;
+    const uniqReg: string[] = uniqRegion(data);
+    const resultOptions: OptionSelect[] = createSelectOptions(uniqReg);
+    return resultOptions;
+  }
+);
 
 const categoryCountriesSlice = createSlice({
-    name: 'category',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchCategory.pending, (state) => {
-            state.isLoading = true
-        })
-        builder.addCase(fetchCategory.fulfilled, (state, action) => {
-            state.options = action.payload
-            state.isLoading = false
-        })
-    }
-})
+  name: 'category',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategory.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchCategory.fulfilled, (state, action) => {
+      state.options = action.payload;
+      state.isLoading = false;
+    });
+  },
+});
 
-export default categoryCountriesSlice.reducer
+export default categoryCountriesSlice.reducer;
